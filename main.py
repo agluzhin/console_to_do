96b7ff207f894751d11fe31933c2aaee17c85e3c
Alex Luzhin <agluzhin@gmail.com>
2024-01-21 17:22:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ logging.basicConfig(level=logging.INFO, filename=f'{__name__}.log', filemode='w'
                     format='%(name)s %(asctime)s %(levelname)s %(message)s')
 
 
-logging.info("Starting the function 'to_do' which imitates to-do list app")
-
-
 def to_do():
+    logging.info("Starting the function 'to_do' which imitates to-do list app")
     tasks = []
 
     logging.info("Starting the cycle inside 'to_do' function")
@@ -20,7 +18,6 @@ def to_do():
         if user_choice == 1:
             new_task = input("Enter your task: ")
             tasks.append(new_task)
-
             logging.info(f"Task '{new_task}' was successfully added")
 
         elif user_choice == 2:
@@ -29,18 +26,15 @@ def to_do():
             for element in tasks:
                 print(element)
             time.sleep(5)
-
             logging.info("All tasks were successfully shown")
 
         elif user_choice == 3:
             task_name = input("Write the task's name: ")
             tasks.remove(task_name)
-
             logging.info(f"Task '{task_name}' was successfully removed")
 
         else:
             print("Thank you for using our app. See you soon.")
-
             logging.info("Cycle succefully broken")
             break
 
